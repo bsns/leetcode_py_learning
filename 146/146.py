c09909aad17df3366b6e70aec03c5ba501f0bbb1
bsns <bsns@live.com>
2023-10-12 15:05:23 +0800

    146

---
+++ 146/146.py
@@ -0,0 +1,146 @@
+#
+#  请你设计并实现一个满足
+#  LRU (最近最少使用) 缓存 约束的数据结构。
+#
+#
+#
+#  实现
+#  LRUCache 类：
+#
+#
+#
+#
+#
+#  LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存
+#  int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+#  void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组
+# key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。
+#
+#
+#
+#
+#  函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
+#
+#
+#
+#  示例：
+#
+#
+# 输入
+# ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+# [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+# 输出
+# [null, null, null, 1, null, -1, null, -1, 3, 4]
+#
+# 解释
+# LRUCache lRUCache = new LRUCache(2);
+# lRUCache.put(1, 1); // 缓存是 {1=1}
+# lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+# lRUCache.get(1);    // 返回 1
+# lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+# lRUCache.get(2);    // 返回 -1 (未找到)
+# lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+# lRUCache.get(1);    // 返回 -1 (未找到)
+# lRUCache.get(3);    // 返回 3
+# lRUCache.get(4);    // 返回 4
+#
+#
+#
+#
+#  提示：
+#
+#
+#  1 <= capacity <= 3000
+#  0 <= key <= 10000
+#  0 <= value <= 10⁵
+#  最多调用 2 * 10⁵ 次 get 和 put
+#
+#
+#  Related Topics 设计 哈希表 链表 双向链表 👍 2973 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class ListNode:
+    def __init__(self,key=None,value=None):
+        self.key = key
+        self.value = value
+        self.pre = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.record = {}
+        self.head = ListNode()
+        self.tail = ListNode()
+        self.head.next= self.tail
+        self.tail.pre = self.head
+
+    def move_node_to_end(self,key):
+        node = self.record[key]
+
+
+        node.next.pre = node.pre
+        node.pre.next = node.next
+
+
+
+        node.pre = self.tail.pre
+        node.next = self.tail
+
+        self.tail.pre.next = node
+        self.tail.pre = node
+
+
+    def get(self, key: int) -> int:
+        if key in self.record:
+            self.move_node_to_end(key)
+        res = self.record.get(key, -1)
+        if (res == -1):
+            return res
+        else:
+            return res.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.record:
+            self.record[key].value = value
+            self.move_node_to_end(key)
+        else:
+            if len(self.record) == self.capacity:
+                self.record.pop(self.head.next.key)
+                self.head.next = self.head.next.next
+                self.head.next.pre = self.head
+
+            new = ListNode(key,value)
+            self.record[key] = new
+
+            new.pre = self.tail.pre
+            new.next = self.tail
+
+            self.tail.pre.next = new
+            self.tail.pre = new
+
+
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+# leetcode submit region end(Prohibit modification and deletion)
+
+
+lRUCache  =  LRUCache(2)
+print(lRUCache.put(1, 1))
+print(lRUCache.put(2, 2))
+print(lRUCache.get(1))
+print(lRUCache.put(3, 3))
+print(lRUCache.get(2))
+print(lRUCache.put(4, 4))
+print(lRUCache.get(1))
+print(lRUCache.get(3))
+print(lRUCache.get(4))
+#
+# 测试结果:[null,null,null,1,null,2,null,-1,3,4]
+# 	期望结果:[null,null,null,1,null,-1,null,-1,3,4]
