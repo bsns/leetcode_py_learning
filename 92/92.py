c7eae0d8bb6fca077ca7976f4348ca2c7481fd48
bsns <bsns@live.com>
2024-05-12 10:54:31 +0800

    Update 92.py
    92

--- 92/92.py
+++ 92/92.py
@@ -37,10 +37,10 @@
 
 # leetcode submit region begin(Prohibit modification and deletion)
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
 
