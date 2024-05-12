3879712f8d474a5f6bdce85dfd4c942b91eb2f69
bsns <bsns@live.com>
2024-05-12 17:00:48 +0800

    Create 674.py
    674

---
+++ 674/674.py
@@ -0,0 +1,59 @@
+# 给定一个未经排序的整数数组，找到最长且 连续递增的子序列，并返回该序列的长度。
+#
+#  连续递增的子序列 可以由两个下标 l 和 r（l < r）确定，如果对于每个 l <= i < r，都有 nums[i] < nums[i + 1] ，那
+# 么子序列 [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] 就是连续递增子序列。
+#
+#
+#
+#  示例 1：
+#
+#
+# 输入：nums = [1,3,5,4,7]
+# 输出：3
+# 解释：最长连续递增序列是 [1,3,5], 长度为3。
+# 尽管 [1,3,5,7] 也是升序的子序列, 但它不是连续的，因为 5 和 7 在原数组里被 4 隔开。
+#
+#
+#  示例 2：
+#
+#
+# 输入：nums = [2,2,2,2,2]
+# 输出：1
+# 解释：最长连续递增序列是 [2], 长度为1。
+#
+#
+#
+#
+#  提示：
+#
+#
+#  1 <= nums.length <= 10⁴
+#  -10⁹ <= nums[i] <= 10⁹
+#
+#
+#  Related Topics 数组 👍 449 👎 0
+
+from typing import List
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        # nums = [1, 3, 5, 4, 7]
+
+        dp = [1 for _ in range(0,len(nums))]
+
+        dp[0] = 1
+        res = 1
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                dp[i] = dp[i - 1] + 1
+                res = max(res,dp[i])
+            # else:
+            #     dp[i] = max(dp[i - 1], 1)
+
+        return res
+
+
+Solution_demo = Solution()
+nums = [1,3,5,4,7]
+res = Solution_demo.findLengthOfLCIS(nums)
+print(res)
