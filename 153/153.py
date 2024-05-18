c4c65ae81285a8ee3a501db4b02c3dc0c2c470bf
bsns <bsns@live.com>
2024-05-18 17:43:09 +0800

    Update 153.py

--- 153/153.py
+++ 153/153.py
@@ -60,27 +60,17 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         # 输入：nums = [4, 5, 6, 7, 0, 1, 2]
         # 输出：0
-        n =  len(nums)
-        left,right=0,n-1
 
-        # left<mid<right
-        #
-        # left<mid  mid>right
-        #
-        # left>mid  mid< right
+        left = 0
+        right = len(nums) - 1
 
-        res = nums[0]
-        while(left<=right):
-            mid = (right-left)//2+left
-            res = min(nums[mid],res)
-            if(nums[left] <= nums[mid] <= nums[right]):
-                right = mid-1
-            elif(nums[left]<=nums[mid] and nums[mid]>=nums[right]):
-                left = mid+1
-            elif(nums[left]>=nums[mid] and nums[mid]<=nums[right]):
-                right=mid-1
-
-        return res
+        while (left < right):
+            mid = (left + right) // 2
+            if (nums[mid] > nums[right]):
+                left = mid + 1
+            else:
+                right = mid
+        return nums[left]
 
 
 Solution_sample = Solution()
