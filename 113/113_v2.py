349b4eadf8ca7c5ff81bcc7464d849a27306b561
bsns <bsns@live.com>
2024-05-18 21:54:33 +0800

    Create 113_v2.py

---
+++ 113/113_v2.py
@@ -0,0 +1,124 @@
+# 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+#
+#  叶子节点 是指没有子节点的节点。
+#
+#
+#
+#
+#
+#
+#
+#  示例 1：
+#
+#
+# 输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+# 输出：[[5,4,11,2],[5,8,4,5]]
+#
+#
+#  示例 2：
+#
+#
+# 输入：root = [1,2,3], targetSum = 5
+# 输出：[]
+#
+#
+#  示例 3：
+#
+#
+# 输入：root = [1,2], targetSum = 0
+# 输出：[]
+#
+#
+#
+#
+#  提示：
+#
+#
+#  树中节点总数在范围 [0, 5000] 内
+#  -1000 <= Node.val <= 1000
+#  -1000 <= targetSum <= 1000
+#
+#
+#  Related Topics 树 深度优先搜索 回溯 二叉树 👍 1055 👎 0
+
+from typing import  Optional,List
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+
+        def dfs(node, currentSum):
+            # 如果节点为空，返回 False
+            if not node:
+                return False
+
+            # 更新当前路径和
+            currentSum += node.val
+
+            # 如果当前节点是叶子节点，检查路径和是否等于目标和
+            if not node.left and not node.right:
+                return currentSum == targetSum
+
+            # 递归检查左右子树
+            return dfs(node.left, currentSum) or dfs(node.right, currentSum)
+
+        # 从根节点开始调用内部函数，初始路径和为 0
+        return dfs(root, 0)
+
+
+Solution_sample = Solution()
+
+# a = TreeNode(1)
+# b = TreeNode(2)
+# c = TreeNode(3)
+# a.left = b
+# a.right = c
+# targetSum = 4
+
+
+
+# [5,4,8,11,null,13,4,7,2,null,null,5,1]
+
+# a = TreeNode(5)
+# b = TreeNode(4)
+# c = TreeNode(8)
+# d = TreeNode(11)
+#
+# e = TreeNode(13)
+# f = TreeNode(4)
+# g = TreeNode(7)
+# h = TreeNode(2)
+#
+# i = TreeNode(5)
+# j = TreeNode(1)
+#
+# a.left = b
+# a.right =c
+# b.left =d
+# d.left=g
+# d.right=h
+#
+# c.left = e
+# c.right = f
+#
+# f.left=i
+# f.right=j
+# targetSum = 22
+
+# 测试结果:[[5,4,11,2],[5,4,8,4,5]]
+# 	期望结果:[[5,4,11,2],[5,8,4,5]]
+
+
+a = TreeNode(-2)
+c = TreeNode(-3)
+
+a.right =c
+targetSum = -5
+# 期望结果:[[-2,-3]]
+res = Solution_sample.pathSum(a,targetSum)
+print(res)
