34736fab84e57f1853a2edac5de1e2f4445d3083
bsns <bsns@live.com>
2024-05-21 14:29:48 +0800

    Update 23.py
    23

--- 23/23.py
+++ 23/23.py
@@ -52,6 +52,8 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+    def __lt__(self, other):
+        return self.val < other.val
 class Solution:
     def mergeKLists(self, lists):
         pass
