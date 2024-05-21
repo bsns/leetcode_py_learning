67a83f5b86c9721118f459f5b36d8382f59b91e5
bsns <bsns@live.com>
2024-05-21 15:00:20 +0800

    Create 19_v2.py
    19

---
+++ 19/19_v2.py
@@ -0,0 +1,90 @@
+# 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+#
+#
+#
+#  示例 1：
+#
+#
+# 输入：head = [1,2,3,4,5], n = 2
+# 输出：[1,2,3,5]
+#
+#
+#  示例 2：
+#
+#
+# 输入：head = [1], n = 1
+# 输出：[]
+#
+#
+#  示例 3：
+#
+#
+# 输入：head = [1,2], n = 1
+# 输出：[1]
+#
+#
+#
+#
+#  提示：
+#
+#
+#  链表中结点的数目为 sz
+#  1 <= sz <= 30
+#  0 <= Node.val <= 100
+#  1 <= n <= sz
+#
+#
+#
+#
+#  进阶：你能尝试使用一趟扫描实现吗？
+#
+#  Related Topics 链表 双指针 👍 2854 👎 0
+
+from typing import Optional
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+
+        dummy_head = ListNode(0)
+        dummy_head.next = head
+        fast = head
+        pre = dummy_head
+
+        for i in range(n):
+            fast = fast.next
+
+        while (fast):
+            fast = fast.next
+            pre = pre.next
+
+        pre.next = pre.next.next
+
+        return dummy_head.next
+
+
+# head = [1,2,3,4,5], n = 2
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+n = 2
+
+
+
+# head = ListNode(1)
+#
+# n = 1
+Solution_Demo = Solution()
+
+res = Solution_Demo.removeNthFromEnd(head,n)
+
+while(res):
+    print(res.val)
+    res=res.next
