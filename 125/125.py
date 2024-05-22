688b13cd07c8caf2f8b534f9d81993ea065c1d26
bsns <bsns@live.com>
2024-05-22 10:29:22 +0800

    Create 125.py

---
+++ 125/125.py
@@ -0,0 +1,87 @@
+# 如果在将所有大写字符转换为小写字符、并移除所有非字母数字字符之后，短语正着读和反着读都一样。则可以认为该短语是一个 回文串 。
+#
+#  字母和数字都属于字母数字字符。
+#
+#  给你一个字符串 s，如果它是 回文串 ，返回 true ；否则，返回 false 。
+#
+#
+#
+#  示例 1：
+#
+#
+# 输入: s = "A man, a plan, a canal: Panama"
+# 输出：true
+# 解释："amanaplanacanalpanama" 是回文串。
+#
+#
+#  示例 2：
+#
+#
+# 输入：s = "race a car"
+# 输出：false
+# 解释："raceacar" 不是回文串。
+#
+#
+#  示例 3：
+#
+#
+# 输入：s = " "
+# 输出：true
+# 解释：在移除非字母数字字符之后，s 是一个空字符串 "" 。
+# 由于空字符串正着反着读都一样，所以是回文串。
+#
+#
+#
+#
+#  提示：
+#
+#
+#  1 <= s.length <= 2 * 10⁵
+#  s 仅由可打印的 ASCII 字符组成
+#
+#
+#  Related Topics 双指针 字符串 👍 740 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        left = 0
+        right = len(s)-1
+
+        while(left<right):
+            while(not  ('a' <= s[left].lower() <='z'  or '0'<=s[left]<='9') and left < right):
+                left+=1
+            while (not ('a' <= s[right].lower() <='z'  or '0'<=s[right]<='9') and left < right):
+                right -= 1
+            if(s[left].lower() != s[right].lower() and left<right):
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Test cases
+test_cases = [
+    # ("A man, a plan, a canal: Panama", True),
+    # ("race a car", False),
+    # (" ", True),
+    # ("0P", False),
+    # ("AAbba", True),
+    # ("abcdefg", False),
+    # ("1234321", True),
+    (".,", False),
+]
+
+for s, expected_result in test_cases:
+    is_palindrome = solution.isPalindrome(s)
+    print(f"Input: '{s}'")
+    print(f"Expected: {expected_result}")
+    print(f"Result: {is_palindrome}")
+    print("-" * 20)
